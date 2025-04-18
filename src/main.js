edc45ef356db226ab744b57cd38bdd74a9eae26f
Sirko Schindler <git@sirko.org>
2025-04-18 13:12:32 +0200

    added SVG export

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/interface.css';
 import '../css/svg.css';
+import SvgCss  from '../css/svg.css?raw';
+
 
 import draw from './draw.js';
 import extract from './extract.js';
@@ -21,7 +23,6 @@ document.querySelector( '#visualize' )
 
       // create the layout
       const layout = await createLayout( content[0] );
-      // document.querySelector( '#dev' ).innerHTML = JSON.stringify( content, null, 2 );
 
       // get SVG container
       const svg = document.querySelector( '#svg' );
@@ -29,17 +30,60 @@ document.querySelector( '#visualize' )
       // draw it
       await draw( svg, layout );
 
+      // remember variable IRI
+      svg.dataset.iri = content[0].getIri();
+
       // enable export button
       document.querySelector( '#export' ).classList.remove( 'invisible' );
 
       // scroll into view
       svg.scrollIntoView( true );
 
-
     } catch( e ) {
       console.error( e );
     }
 
   });
-
 document.querySelector( '#visualize' ).click();
+
+document.querySelector( '#export' )
+  .addEventListener( 'click', async (e) => {
+
+    // only trigger on options not the select itself
+    if( e.target.tagName.toUpperCase() != 'A' ) {
+      return;
+    }
+
+    // output depends on type
+    switch( e.target.dataset.format ) {
+
+      case 'svg':
+
+        // get iri and derive filename from it
+        const svg = document.querySelector( '#svg' );
+        const iri = svg.dataset.iri;
+        const filename = iri.split( /[/#]/ ).pop() + '.svg';
+
+        // get SVG content and prepare for download
+        let content = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n' + svg?.innerHTML;
+        content = content
+          .replace( '<svg', '<svg xmlns="http://www.w3.org/2000/svg"' )
+          .replace( '<defs>', `<defs><style>${SvgCss}</style>`);
+
+        // download
+        const svgBlob = new Blob([content], {type: 'image/svg+xml;charset=utf-8' });
+        const svgUrl = URL.createObjectURL(svgBlob);
+        const downloadLink = document.createElement('a');
+        downloadLink.href = svgUrl;
+        downloadLink.download = filename;
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+
+        break;
+
+      default: throw Error( 'Unknown export format!' );
+
+    }
+
+  });
